744b1591d121e828291c9d18ac07809c42ab799f
Björn Tirsén <bjorntirsen@gmail.com>
2020-09-20 16:23:01 +0200

    Fixed the bugs with permanent delete

--- script.js
+++ script.js
@@ -3,7 +3,38 @@ document.addEventListener("DOMContentLoaded", function(event) {
     let lista2 = document.getElementById("lista2");
     let lista3 = document.getElementById("lista3");
     let nummer = 0;
-    let syssla = document.getElementById("syssla");
+    let syssla = document.getElementById("syssla");    
+    //Funktion för att lägga till permanent borttagning
+    function removeButtons(target) {
+        let a = target.parentNode;
+        a.removeChild(a.lastChild);
+        a.removeChild(a.lastChild);        
+        return a;
+    }
+    //Funktion för att flytta till annan lista
+    function moveTo(parentNode, destination) {
+        let a = parentNode;
+        let b = a.cloneNode(true);
+        destination.appendChild(b);
+        parentNode.remove();
+        let c = b;
+        return c;
+    }
+    //Skapar permanent ta bort knapp
+    function addPermDelete(target) {
+        let pdel_button = document.createElement("span");
+        pdel_button.innerHTML = "Ta bort helt";
+        pdel_button.classList.add("pdelete");
+        pdel_button.setAttribute("id", "pdel_" +nummer);
+        target.appendChild(pdel_button);
+        let deletes = document.querySelectorAll(".pdelete")
+        deletes.forEach(btn => {
+            btn.addEventListener("click", function() {
+                let li = this.parentNode;
+                li.remove();
+            })
+        })
+    }    
     //Koden nedan triggas när man trycker Enter när något
     //tecken finns i textrutan.
     document.addEventListener("keypress", function(e) {
@@ -18,7 +49,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
             let lista = document.getElementById("lista");
             lista.appendChild(grej);
             grej.setAttribute("id", "grej_" + nummer);
-
+                   
             //Skapar en klart knapp & lägger till i li
             let done_button = document.createElement("span");
             done_button.innerHTML = "Klart";
@@ -27,18 +58,10 @@ document.addEventListener("DOMContentLoaded", function(event) {
             grej.appendChild(done_button);
             //Lägger på en lyssnare på klart knappen
             let done_span = document.getElementById("done_" +nummer);
-            done_span.addEventListener("click", function() {
-                //Kopierar li till Färdigt
-                let done1 = this.parentNode;
-                let done2 = done1.cloneNode(true);
-                lista2.appendChild(done2);
-                //Tar bort li från första listan
-                this.parentNode.remove();  
-                //Skapar lyssnare för permanent borttagning
-                let pdelete_span = document.getElementById("del_" +nummer);
-                pdelete_span.addEventListener("click", function() {
-                    this.parentNode.remove();
-                })
+            done_span.addEventListener("click", function(event){
+                a = removeButtons(event.target);
+                b = moveTo(a, lista2);
+                addPermDelete(b);                
             })
 
             //Skapar en ta bort knapp & lägger till i li
@@ -49,18 +72,10 @@ document.addEventListener("DOMContentLoaded", function(event) {
             grej.appendChild(delete_button);
             //Lägger på en lyssnare på ta bort knappen
             let delete_span = document.getElementById("del_" +nummer);
-            delete_span.addEventListener("click", function() {
-                //Kopierar li till borttagna
-                let borttagen1 = this.parentNode;
-                let borttagen2 = borttagen1.cloneNode(true);
-                lista3.appendChild(borttagen2);
-                //Tar bort li från första listan
-                this.parentNode.remove();
-                //Skapar lyssnare för permanent borttagning
-                let pdelete_span2 = document.getElementById("del_" +nummer);
-                pdelete_span2.addEventListener("click", function() {
-                    this.parentNode.remove();
-                })
+            delete_span.addEventListener("click", function(event) {
+                a = removeButtons(event.target);
+                b = moveTo(a, lista3);
+                addPermDelete(b);
             })
             //Nollställer textfältet
             syssla.value = "";
